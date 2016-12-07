a4a11dedba861be9971d11de7858aebb9b48b22a
tomas.pavel@etnetera.cz <724137277>
2016-12-07 19:19:16 +0100

    parsovani parametru programu

--- src/main/java/cz/kamenitxan/jakon/Main.scala
+++ src/main/java/cz/kamenitxan/jakon/Main.scala
@@ -13,6 +13,15 @@ object Main {
 	private val logger = LoggerFactory.getLogger(this.getClass)
 
 	def main(args: Array[String]) {
+		val arguments = args.toList.map(a => {
+			val split = a.split("=")
+			split.length match {
+				case 1 => split(0) -> None
+				case 2 => split(0) -> split(1)
+			}
+		})
+
+
 		logger.info("Jakon started")
 		//Director.init()
 		DBHelper.addDao(new JakonUser().getClass)
