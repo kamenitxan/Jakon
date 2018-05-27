b65d87cc73e9218143dbd491ed49a43dcc21d5ff
Tomáš Pavel <tomas.pavel@etnetera.cz>
2018-05-27 11:40:55 +0200

    i18n

--- src/main/java/cz/kamenitxan/jakon/webui/functions/i18nFun.scala
+++ src/main/java/cz/kamenitxan/jakon/webui/functions/i18nFun.scala
@@ -11,10 +11,14 @@ import com.mitchellbosecke.pebble.template.EvaluationContext
 import cz.kamenitxan.jakon.core.configuration.Settings
 
 class i18nFun extends i18nFunction {
+
+	getArgumentNames.add("def")
+
 	override def execute(args: util.Map[String, AnyRef]): AnyRef = {
 		val basename = args.get("bundle").asInstanceOf[String]
 		val key = args.get("key").asInstanceOf[String]
 		val params = args.get("params")
+		val default = args.get("def").asInstanceOf[String]
 
 		val context = args.get("_context").asInstanceOf[EvaluationContext]
 		val locale = if (Settings.getDefaultLocale == null) context.getLocale else Settings.getDefaultLocale
@@ -26,11 +30,23 @@ class i18nFun extends i18nFunction {
 		val bundle = ResourceBundle.getBundle(basename, locale, loader, new UTF8Control)
 		var phraseObject = ""
 		try {
-			phraseObject = bundle.getString(key)
-		} catch {
-			case ex: MissingResourceException => {
-				val bundle = ResourceBundle.getBundle(basename, new Locale("en", "US"), loader, new UTF8Control)
+			if (bundle.containsKey(key)) {
 				phraseObject = bundle.getString(key)
+			} else {
+				phraseObject = bundle.getString(default)
+			}
+		} catch {
+			case _: MissingResourceException => {
+				val bundle = ResourceBundle.getBundle(basename, new Locale("en", "US"), loader, new UTF8Control)
+				try {
+					if (bundle.containsKey(key)) {
+						phraseObject = bundle.getString(key)
+					} else {
+						phraseObject = bundle.getString(default)
+					}
+				} catch {
+					case _: MissingResourceException => phraseObject = if (default != null && !default.isEmpty) default else key
+				}
 			}
 		}
 
