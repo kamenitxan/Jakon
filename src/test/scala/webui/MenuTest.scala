8a1f2ea9919b0313a17201db2502cffaaabf2eef
tomas.pavel@etnetera.cz <tomas.pavel@etnetera.cz>
2018-08-16 17:54:54 +0200

    selenium

--- src/test/scala/webui/MenuTest.scala
+++ src/test/scala/webui/MenuTest.scala
@@ -1,17 +1,29 @@
 package webui
 
 import cz.kamenitxan.jakon.core.configuration.Settings
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.scalatest.FunSuite
+import org.openqa.selenium.firefox.{FirefoxDriver, FirefoxOptions}
+import org.scalatest.{Outcome, fixture}
 
-class MenuTest extends FunSuite  {
+class MenuTest extends fixture.FunSuite  {
+	val host = "http://localhost:"  + (Settings.getPort -1)  + "/admin/"
 
 
-	test("menu items") {
-		val host = "http://localhost:"  + (Settings.getPort -1)  + "/admin/"
-		val driver = new FirefoxDriver()
-		driver.get(host + "index")
+	case class FixtureParam(driver: FirefoxDriver)
 
-		driver.close()
+	def withFixture(test: OneArgTest): Outcome = {
+		val opt = new FirefoxOptions
+		opt.addArguments("-headless")
+		val driver = new FirefoxDriver(opt)
+
+		val fixture = FixtureParam(driver)
+		try {
+			withFixture(test.toNoArgTest(fixture)) // "loan" the fixture to the test
+		}
+		finally driver.close()
+	}
+
+
+	test("menu items") { f =>
+		f.driver.get(host + "index")
 	}
 }
