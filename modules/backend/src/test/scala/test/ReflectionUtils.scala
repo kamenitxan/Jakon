ac8f4147d1c89351ae4588b2074e63b4212c92a6
kamenitxan <tomas.sibe@gmail.com>
2022-08-14 14:01:10 +0200

    testy

--- modules/backend/src/test/scala/test/ReflectionUtils.scala
+++ modules/backend/src/test/scala/test/ReflectionUtils.scala
@@ -7,24 +7,13 @@ object ReflectionUtils {
 
 	def changeFieldValue(fieldName: String, inst: AnyRef, value: Any): Unit = {
 		val field = inst.getClass.getDeclaredField(fieldName)
+		field.setAccessible(true)
 		field.set(inst, value)
 	}
 
 	def changeObjectFieldValue(fieldName: String, objectName: String, value: Any): Unit = {
-		???
-		/*import scala.reflect.runtime.{universe => ru}
-		val mirror = ru.runtimeMirror(getClass.getClassLoader)
-
-		val moduleSymbol = mirror.staticModule(objectName)
-		val moduleMirror = mirror.reflectModule(moduleSymbol)
-		val instanceMirror = mirror.reflect(moduleMirror.instance)
-
-		val field = moduleSymbol.typeSignature.declarations
-		  .filter(d => d.asTerm.isVal || d.asTerm.isVar)
-		  .filter(f => f.name.toString.strip() == fieldName)
-		  .head
-		val fieldMirror = instanceMirror.reflectField(field.asTerm)
-		fieldMirror.set(value)*/
-
+		val cls = Class.forName(objectName+"$")
+		val instance = cls.getDeclaredField("MODULE$").get(null)
+		changeFieldValue(fieldName, instance, value)
 	}
 }
