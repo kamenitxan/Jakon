174de533d3262fc0c74b6ccd3d89076464bdfccd
Kamenitxan <kamenitxan@me.com>
2024-06-08 17:13:48 +0200

    remove brunch

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
 let mix = require('laravel-mix');
 
 mix.js([
-	'src/frontend/admin/js/jakon.js',
-	'src/frontend/admin/js/forms/j_textarea.js'
+	'modules/frontend/src/main/js/jakon.js',
+	'modules/frontend/src/main/js/forms/j_textarea.js'
 ], 'js/jakon.js')
-	.css('src/frontend/admin/css/jakon.css', 'css/jakon.css')
+	.css('modules/frontend/src/main/css/jakon.css', 'css/jakon.css')
 	.autoload({
 		jquery: ['$', 'window.jQuery']
 	})
