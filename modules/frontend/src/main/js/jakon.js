c695b6cfd4b890673734e89a1953627a1db58400
Kamenitxan <kamenitxan@me.com>
2025-07-21 17:22:13 +0200

    Migrate from Laravel Mix to Vite for build process and update dependencies accordingly.

--- modules/frontend/src/main/js/jakon.js
+++ modules/frontend/src/main/js/jakon.js
@@ -1,4 +1,6 @@
+import 'vite/modulepreload-polyfill'
 import JTextarea from "./forms/j_textarea";
+import '../css/jakon.css';
 
 window.jakon = {};
 window.jakon.JTextarea = JTextarea;
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import {defineConfig} from "vite";
+
+export default defineConfig({
+	server: {
+		cors: {
+			// the origin you will be accessing via browser
+			origin: 'http://localhost:4567',
+		},
+	},
+	build: {
+		// generate .vite/manifest.json in outDir
+		manifest: true,
+		rollupOptions: {
+			// overwrite default .html entry
+			input: 'modules/frontend/src/main/js/jakon.js',
+			output: {
+				dir: 'modules/backend/src/main/resources/static/jakon/vite',
+				entryFileNames: 'jakon.js',
+				assetFileNames: (assetInfo) => {
+					if (assetInfo.name === 'style.css') return 'jakon.css';
+					return '[name].[ext]';
+				},
+			}
+		},
+		cssCodeSplit: false,
+	},
+});
+
