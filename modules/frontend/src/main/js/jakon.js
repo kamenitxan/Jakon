5b80136e7c7d68f615cd8908c6b9396a49462a59
Kamenitxan <kamenitxan@me.com>
2025-11-28 11:01:47 +0100

    replaced markdown editor with new one

--- modules/frontend/src/main/js/jakon.js
+++ modules/frontend/src/main/js/jakon.js
@@ -1,11 +1,9 @@
 import 'vite/modulepreload-polyfill'
-import JTextarea from "./forms/j_textarea";
 import '../css/jakon.css';
 import jQuery from 'jquery';
 
 
 window.jakon = {};
-window.jakon.JTextarea = JTextarea;
 
 window.$ = jQuery;
 window.jQuery = jQuery;
