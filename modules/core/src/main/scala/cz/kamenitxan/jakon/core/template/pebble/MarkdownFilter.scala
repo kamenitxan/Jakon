b831679871c5386c9ad624400bfb86a118e85843
Kamenitxan <kamenitxan@me.com>
2026-02-16 20:56:50 +0100

    support GFM extensions in MarkdownFilter; bump dependency versions in build.sbt

--- modules/core/src/main/scala/cz/kamenitxan/jakon/core/template/pebble/MarkdownFilter.scala
+++ modules/core/src/main/scala/cz/kamenitxan/jakon/core/template/pebble/MarkdownFilter.scala
@@ -3,6 +3,8 @@ package cz.kamenitxan.jakon.core.template.pebble
 import cz.kamenitxan.jakon.core.template.function.FunctionHelper
 import io.pebbletemplates.pebble.extension.Filter
 import io.pebbletemplates.pebble.template.{EvaluationContext, PebbleTemplate}
+import org.commonmark.ext.gfm.strikethrough.StrikethroughExtension
+import org.commonmark.ext.gfm.tables.TablesExtension
 import org.commonmark.parser.Parser
 import org.commonmark.renderer.html.HtmlRenderer
 
@@ -19,14 +21,16 @@ class MarkdownFilter extends Filter {
 
 object MarkdownFilter {
 
+	private val extensions = util.List.of(TablesExtension.create, StrikethroughExtension.create)
+	private val parser = Parser.builder.extensions(extensions).build
+	private val renderer = HtmlRenderer.builder
+		.extensions(extensions)
+		.softbreak("<br>")
+		.build
+
 	def parseString(input: String): String = {
-		val parser = Parser.builder.build
 		val document = parser.parse(input)
-		val renderer = HtmlRenderer.builder.build
 		val renderedString = renderer.render(document)
-		  .replace("\r\n", "<br>")
-		  .replace("\n", "<br>")
-		  .replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;")
 		val result = FunctionHelper.parse(renderedString)
 		result
 	}
