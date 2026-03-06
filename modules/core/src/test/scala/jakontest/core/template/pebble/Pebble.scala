227f1d784422d621d2b86337cc0ce7e3eea2797e
Kamenitxan <kamenitxan@me.com>
2026-03-06 17:21:22 +0100

    make test content and expected content private in Pebble test

--- modules/core/src/test/scala/jakontest/core/template/pebble/Pebble.scala
+++ modules/core/src/test/scala/jakontest/core/template/pebble/Pebble.scala
@@ -7,9 +7,9 @@ import org.scalatest.funsuite.AnyFunSuite
 @DoNotDiscover
 class Pebble extends AnyFunSuite {
 
-	val testContent = "```\nclass ExampleTask(period: Long, unit: TimeUnit) extends AbstractTask(classOf[ExampleTask].getSimpleName, period, unit) {  " +
+	private val testContent = "```\nclass ExampleTask(period: Long, unit: TimeUnit) extends AbstractTask(classOf[ExampleTask].getSimpleName, period, unit) {  " +
 	  "\n\toverride def start(): Unit = {\n\t\t???\n\t}\n}\n```\n\nThen in JakonInit\n\n```\ndef taskSetup(): Unit = {\n\t\tTaskRunner.registerTask(new ExampleTask(10, TimeUnit.MINUTES))\n}\n```\n"
-	val expectedContent = "<pre><code>class ExampleTask(period: Long, unit: TimeUnit) extends AbstractTask(classOf[ExampleTask].getSimpleName, period, unit) {  <br>&nbsp;&nbsp;&nbsp;&nbsp;override def start(): Unit = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;???<br>&nbsp;&nbsp;&nbsp;&nbsp;}<br>}<br></code></pre><br><p>Then in JakonInit</p><br><pre><code>def taskSetup(): Unit = {<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TaskRunner.registerTask(new ExampleTask(10, TimeUnit.MINUTES))<br>}<br></code></pre><br>"
+	private val expectedContent = "<pre><code>class ExampleTask(period: Long, unit: TimeUnit) extends AbstractTask(classOf[ExampleTask].getSimpleName, period, unit) {  \n\toverride def start(): Unit = {\n\t\t???\n\t}\n}\n</code></pre>\n<p>Then in JakonInit</p>\n<pre><code>def taskSetup(): Unit = {\n\t\tTaskRunner.registerTask(new ExampleTask(10, TimeUnit.MINUTES))\n}\n</code></pre>\n"
 
 	test("markdownFilter") {
 		val res = MarkdownFilter.parseString(testContent)
